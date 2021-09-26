df1b9dafc4423a63943e42cbb3aea889e6657cd9
Ieuan Skinner <ieuanskinner@gmail.com>
2021-09-26 20:48:12 +0100

    Added key property to array of ReactDOM elements.

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ export default function App() {
         Is <input id={'palindrome-input'} type={'text'} placeholder={'This'} onChange={e => setText(e.target.value)}/> A Palindrome?
       </div>
       { text.length > 0 ? [
-        <div className={'checkbox-control'}>
+        <div className={'checkbox-control'} key={'checkbox-control'}>
           <div>
             <label htmlFor='case-sensitive-input'>Case Sensitive</label>
             <input id={'case-sensitive-input'} type={'checkbox'} value={''} onChange={e => setCaseSensitive(e.target.checked)}
@@ -25,7 +25,8 @@ export default function App() {
                    aria-label={'Checkbox to control removing of spaces'} />
           </div>
         </div>,
-        <Palindrome text={removeSpaces ? text.replaceAll(/\s+/g, '') : text} caseSensitive={caseSensitive} />
+        <Palindrome key={'palindrome'} text={removeSpaces ? text.replaceAll(/\s+/g, '') : text}
+                    caseSensitive={caseSensitive} />
       ] : ''}
     </div>
   )
